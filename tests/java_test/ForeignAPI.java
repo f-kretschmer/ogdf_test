291c9ac385454a56c3417a93283df0406578f785
LukasScholz <l.kas.scholz@gmail.com>
2024-01-15 20:48:48 +0100

    Added Method Handles and invokes until Layout Function call

--- tests/java_test/ForeignAPI.java
+++ tests/java_test/ForeignAPI.java
@@ -88,36 +88,82 @@ public class ForeignAPI {
 
         try (Arena offHeap = Arena.ofConfined()) {
 
+            // Declare native std functions
+            MethodHandle makeTuple;
+            MethodHandle makeEmptyVector;
+            MethodHandle appendtoVector;
+            MethodHandle getTupleElement;
+            MethodHandle getVectorElement;
 
+            // Define native functions
+            //TODO: Std function names differ from expected.
+
+            /*
+            makeTuple = linker.downcallHandle(stdlib.find("make_tuple").orElseThrow(),
+                    // std::tuple<uint32_t, uint32_t, float>
+                    FunctionDescriptor.of(ADDRESS, JAVA_INT, JAVA_INT, JAVA_FLOAT));
+
+            makeVector = linker.downcallHandle(stdlib.find("vector").orElseThrow(),
+                    // std::vector<std::tuple>
+                    FunctionDescriptor.of(ADDRESS, ADDRESS));
+
+            appendtoVector = linker.downcallHandle(stdlib.find("push_back").orElseThrow(),
+                    // std::vector.push_back(std::tuple)
+                    FunctionDescriptor.of(ADDRESS, ADDRESS));
+
+            getTupleElement = linker.downcallHandle(stdlib.find("get").orElseThrow(),
+                    // std::get<i>(tuple) -> i als Position in Tuple
+                    FunctionDescriptor.of(ADDRESS, JAVA_INT, ADDRESS));
+
+            getVectorElement = linker.downcallHandle(stdlib.find("").orElseThrow(),
+                    // std::vector[i]
+                    FunctionDescriptor.of());
+            */
 
             // load our native lib
             SymbolLookup tmap = SymbolLookup.libraryLookup(libNames.tmapFileName, offHeap);
             SymbolLookup lib = SymbolLookup.libraryLookup("libLayoutFromEdgeList_internal.so", offHeap);
+
             // get tmap::LayoutConfiguration
-            // TODO: -> optionaler parameter?
+            // TODO: -> Notwendig da optionaler parameter?
 
-            // make edges
-            // TODO: copy from Java Arrays (see above)
-            // Allocate off-heap memory to store pointers
-            MemorySegment input1mem = offHeap.allocateArray(JAVA_INT, 1, 2, 3, 4, 1);
-            MemorySegment input2mem = offHeap.allocateArray(JAVA_INT, 2, 3, 4, 1, 3);
-            MemorySegment input3mem = offHeap.allocateArray(JAVA_FLOAT, 0.5F, 1.0F, 0.5F, 1.0F, 1.2F);
-
-
-
-            // reserve output vector
-            // TODO:
+            // TODO: make edges
+            // MemorySegment firstEdge = (MemorySegment) makeTuple.invoke(input1[0], input2[0], weights[0]);
+            // MemorySegment edgeVector = (MemorySegment) makeVector.invoke(firstEdge);
+            for(int i=1; i<weights.length; i++) { //Eintrag 0 schon in Vector?
+                // MemorySegment edge = (MemorySegment) makeTuple.invoke(input1[i], input2[i], weights[i]);
+                // MemorySegment edgeVector = (MemorySegment) appendtoVector.invoke(edge);
+            }
 
             // get handle for layout fun
             /*
             MethodHandle tmapLayout = Linker.nativeLinker().downcallHandle(
                     tmap.find(FunctionNames.tmapLayoutFromEdgeList).orElseThrow(),
                     // returns tuple of vectors and GraphProperties, input: int, edges (address), config(adress), bool, bool
-                    //FunctionDescriptor.of(ADDRESS, JAVA_INT, ADDRESS, JAVA_BOOLEAN, JAVA_BOOLEAN));
+                    // FunctionDescriptor.of(ADDRESS, JAVA_INT, ADDRESS, JAVA_BOOLEAN, JAVA_BOOLEAN));
+                    // Config(address), bool, bool might be optional
+                    // optional Descriptor
                     FunctionDescriptor.of(ADDRESS, JAVA_INT, ADDRESS));
             */
-            // call
-            //tmapLayout.invoke();
+            // call Function
+            // MemorySegment result = tmapLayout.invoke(weights.length, edgeVector);
+
+            // Extract results from Pointer
+            // TODO:
+
+
+
+
+
+            /**
+             *  Alternative if we can include the custom script
+             *
+             **/
+
+            // Allocate off-heap memory to store pointers
+            MemorySegment input1mem = offHeap.allocateArray(JAVA_INT, 1, 2, 3, 4, 1);
+            MemorySegment input2mem = offHeap.allocateArray(JAVA_INT, 2, 3, 4, 1, 3);
+            MemorySegment input3mem = offHeap.allocateArray(JAVA_FLOAT, 0.5F, 1.0F, 0.5F, 1.0F, 1.2F);
 
             MethodHandle libLayout = Linker.nativeLinker().downcallHandle(
                     lib.find("_Z28LayoutFromEdgeList_internalsiPiS_Pfi").orElseThrow(),
@@ -126,6 +172,8 @@ public class ForeignAPI {
                     FunctionDescriptor.of(ADDRESS, JAVA_INT, ADDRESS, ADDRESS, ADDRESS, JAVA_INT));
 
             MemorySegment result = (MemorySegment) libLayout.invoke(5, input1mem, input2mem, input3mem, 6);
+            // Note: prints 0 to console, because I forgot to remove the print of GraphProperties.mst_weight (line 12)
+
 
             // Extract results from Pointer
             //TODO
