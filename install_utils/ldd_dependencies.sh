f2ea8be28b397c8fcc5261641559d2d541b8b4b9
Fleming Kretschmer <fleming.kretschmer@uni-jena.de>
2024-03-06 14:43:47 +0100

    get dependencies for all OSes; fix for windows

--- install_utils/ldd_dependencies.sh
+++ install_utils/ldd_dependencies.sh
@@ -2,11 +2,21 @@
 
 pwd
 mkdir dependencies
+
+# write dependencies to temp file
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    ldd *.so | grep -v 'not found' > lddout
+elif [[ "$OSTYPE" == "msys" ]]; then
+    ldd *.dll | grep -v 'not found' > lddout
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    chmod +x ../install_utils/mac_ldd.sh
+    ../install_utils/mac_ldd.sh *.dylib > lddout
+fi
 # for more libraries, just add another '-e' part with the corresponding pattern
-ldd libCOIN.so | grep -v 'not found' | grep -e 'stdc++' \
-                                            -e 'gcc_s' \
-                                            -e 'omp' \
-                                            -e 'COIN' \
-                                            -e 'OGDF' \
-                                            -e 'tmap' \
+cat lddout | grep -e 'c++' \
+                  -e 'gcc_s' \
+                  -e 'omp' \
+                  -e 'COIN' \
+                  -e 'OGDF' \
+                  -e 'tmap' \
     | cut -d' ' -f3 | xargs -r0 cp -t dependencies
